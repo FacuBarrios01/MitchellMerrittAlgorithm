c1557f3c553f76b8e6f56ad83280277e91eacfc8
FacuBarrios01 <facubarrios@gmail.com>
2023-06-06 19:23:21 +0200

    Commit before fucking all up with a HashMap

--- MitchellMerritt.java
+++ MitchellMerritt.java
@@ -1,12 +1,138 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MitchellMerritt {
+    private static int adjMatrix[][];
+    private static Map<Integer, Process> allProcess = new HashMap<Integer, Process>();
+    private static int numProcesses;
+    private static List<Integer> existingValues = new ArrayList<>();
 
-    public static void main(String args[]) throws IOException{
+    // Add edge from i to j
+    public static void addEdge(int i, int j) {
+        adjMatrix[i][j] = 1;
+    }
+
+    // Remove edge from i to j
+    public static void removeEdege(int i, int j) {
+        adjMatrix[i][j] = 0;
+    }
+
+    public static void printMatrix() {
+        System.out.println("System state: ");
+        for (int i = 0; i < numProcesses; i++) {
+            for (int j = 0; j < numProcesses; j++) {
+                System.out.print(adjMatrix[i][j]);
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
+
+    //
+    /**
+     * 
+     * @param processId
+     * @return Returns the processes that are blocking the process with the
+     *         processId
+     */
+    public static List<Integer> getProcessDependencies(int processId) {
+        List<Integer> dependencies = new ArrayList<Integer>();
+        for (int i = 0; i < numProcesses; i++) {
+            if (adjMatrix[processId][i] == 1)
+                dependencies.add(i);
+        }
+        return dependencies;
+    }
+
+    /**
+     * 
+     * @param processId
+     * @return Returns processes that are blocked by the process with the processId
+     */
+    public static List<Integer> getBlockedByProcess(int processId) {
+        List<Integer> dependencies = new ArrayList<Integer>();
+        for (int i = 0; i < numProcesses; i++) {
+            if (adjMatrix[i][processId] == 1)
+                dependencies.add(i);
+        }
+        return dependencies;
+    }
+
+    /**
+     * 
+     * @param i process been blocked
+     * @param j blocker process
+     */
+    public static void blockStep(int i, int j) {
+        Process initiatorProcess = allProcess[i];
+        Process blockerProcess = allProcess[j];
+        int newLabel = Math.max(initiatorProcess.getPublicId(), blockerProcess.getPublicId()) + 1;
+        initiatorProcess.setPrivateId(newLabel);
+        initiatorProcess.setPublicId(newLabel);
+    }
+
+    public static void tramsmitStep() {
+        for (int i = 0; i < numProcesses; i++) {
+            Process process = allProcess[i];
+            int processId = i;
+            List<Integer> blockerProcesses = getProcessDependencies(processId);
+            blockerProcesses.forEach(blockerId -> {
+                Process blockerProcess = allProcess[blockerId];
+                if (blockerProcess.getPublicId() > process.getPublicId()) {
+                    process.setPublicId(processId);
+                }
+            });
+        }
+    }
+
+    /**
+     * If there is a cycle detected, returns true, if not, it return false
+     * 
+     * @return cycle detected
+     */
+    public static boolean detectStep() {
+        int result = -1;
+        for (int i = 0; i < numProcesses; i++) {
+            Process process = allProcess[i];
+            int processId = process.getPrivateId();
+            List<Integer> blockerProcesses = getProcessDependencies(processId);
+            for (int blockerId : blockerProcesses) {
+                Process blockerProcess = allProcess[blockerId];
+                if (blockerProcess.getPublicId() == process.getPublicId()) {
+                    result = processId;
+                    System.out.println("Block detected by process " + result);
+                    System.out.println("Process " + process.getPrivateId() + " public id= " + process.getPublicId());
+                    System.out.println("Blocker process " + blockerProcess.getPrivateId() + " public id= "
+                            + blockerProcess.getPublicId());
+                    System.out.print("\n");
+                }
+            }
+        }
+        return (result != -1) ? true : false;
+    }
+
+    // Creation of processes with random id's in the range [1 - 50]
+    public static void createProcess(int processId) {
+        Random random = new Random();
+        int newProcessLabels;
+
+        do {
+            newProcessLabels = random.nextInt(50) + 1;
+        } while (existingValues.contains(newProcessLabels));
+        // Add existing values to the list
+        existingValues.add(newProcessLabels);
+        allProcess[processId] = new Process(newProcessLabels);
+    }
+
+    public static void main(String args[]) throws IOException {
         // Check for correct arguments
-        int numProcesses;
         File inputFile = null;
         Scanner reader = null;
         String line;
@@ -31,6 +157,56 @@ public class MitchellMerritt {
         }
         numProcesses = Integer.parseInt(line.split(":")[1].trim());
 
+        // Now that we know the dimensio of our system we give a size to our varaibles
+        adjMatrix = new int[numProcesses][numProcesses];
+        allProcess = new Process[numProcesses];
+
+        // Ingest our initial adj. matrix state
+        int adjMatrixIndex = 0;
+        String edges[];
+        while (reader.hasNextLine()) {
+            tramsmitStep();
+            line = reader.nextLine();
+            // check for end of section
+            if (line.contains("--"))
+                break;
+
+            edges = line.split(" ");
+            for (int i = 0; i < edges.length; i++) {
+                adjMatrix[adjMatrixIndex][i] = Integer.parseInt(edges[i]);
+
+                // Create processes if they were not already initialized
+                if (Objects.isNull(allProcess[adjMatrixIndex]))
+                    createProcess(adjMatrixIndex);
+                if (Objects.isNull(allProcess[i]))
+                    createProcess(i);
+
+                if (adjMatrix[adjMatrixIndex][i] == 1)
+                    blockStep(adjMatrixIndex, i);
+            }
+            adjMatrixIndex++;
+        }
+
+        // Print initial state and execute transmition and detect steps
+        printMatrix();
+        tramsmitStep();
+        detectStep();
+
+        // Ingest new edges,
+        // print matrix state
+        // and print detectec cycles in case there are any
+        while (reader.hasNextLine()) {
+            line = reader.nextLine();
+            String processesIds[] = line.split("-");
+            int processId = Integer.parseInt(processesIds[0].trim());
+            int blockerId = Integer.parseInt(processesIds[1].trim());
+            addEdge(processId, blockerId);
+            blockStep(processId, blockerId);
+            printMatrix();
+            if (detectStep())
+                break;
+        }
         reader.close();
+        System.out.println("Execution completed");
     }
 }
--- README.md
+++ README.md
@@ -10,7 +10,8 @@ In vscode you can use the integrated previewer with the key combination (Ctrl + 
 
 ## Dataset
 First line must contain "Number of processes:" immediately followed by a integer value ex."Number of processes:7".
-Starting from the next line we describe our graph as a Adjacency Matrix
+Starting from the next line we describe our graph as a Adjacency Matrix.
+To mark the end of the adjency matrix we will introduce a line with at lest two consecutive dashes. ie. "--".
 Afther describing the initial state of the graph, we will introduce new edges to our graph representing dependencies.
 We will add them with the following format: $DEPENDING_PROCESS - $BLOCKER_PROCESS
 The first value must be the depending process id, followed by a '-' and the id of the blocker process. Ej.: 2 - 3
@@ -18,4 +19,9 @@ Ex. "Process 2 is waiting for a resource blocked by process 3".
 Process id MUST NOT exceed the value of the number of processes. See example dataset in the documentation.
 
 ## Code coments
+
+To demonstrate the functioning of this algorithm we will provide the program with a initial system state represented by a adjency graph. 
+After defining out initial state we will introduce new dependencies to our system and evaluate is those changes generate a deadlock.
+Dataset1 will indoruce new dependencies that will not generate a deadlock. Meanwhile Dataset 2 will introduce changes that are sopoused to generate a deadlock.  
+
 For a better understanding of the code I recomend start reviewing it starting from the main method inside the MitchellMerritt.java file
